faa6101a8f38fa9238a833f4c045bc2b9fd85e90
Chuyini <jmlr231201@gmail.com>
2024-06-12 10:58:37 -0600

    Version estable de prueba version 3

--- src/service/whatsappService.js
+++ src/service/whatsappService.js
@@ -1,9 +1,7 @@
-const https=require("https");
-const { options } = require("../routes/routes");
+const https = require("https");
 
-function SendMessageWhatsApp(textResponse,number){
-
-    const data=JSON.stringify({
+function SendMessageWhatsApp(textResponse, number) {
+    const data = JSON.stringify({
         "messaging_product": "whatsapp",    
         "recipient_type": "individual",
         "to": number,
@@ -14,39 +12,34 @@ function SendMessageWhatsApp(textResponse,number){
         }
     });
 
-    const options={
-        host:"graph.facebook.com",
-        path:"/v19.0/321806707686253/messages",
-        method:"POST",
-        body: data,
-        headers:{
-            "Content-Type":"application/json",
-            Authorization:"Bearer EAAGeKA2VNZBEBO4QRYJRofcoZBBF8opzbqrSnsqXm0MpaDqMvp53KRRn3euZBISAHeLb0wsqsSzCWnjayUSxOr0yVYaCGfKEnQxwkqSvUi0LuuRmiRYqonrHSdxZBv0LjDIsS1MfnnW1pyo9ejnUYDPTBpojLEt5ZBAgZCCbUQ9Eof1xnsr8h9d0b3bWY6b6aNqrOTZA7jZC5gKZApbZB0"
+    const options = {
+        host: "graph.facebook.com",
+        path: "/v19.0/321806707686253/messages",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer EAAGeKA2VNZBEBO4QRYJRofcoZBBF8opzbqrSnsqXm0MpaDqMvp53KRRn3euZBISAHeLb0wsqsSzCWnjayUSxOr0yVYaCGfKEnQxwkqSvUi0LuuRmiRYqonrHSdxZBv0LjDIsS1MfnnW1pyo9ejnUYDPTBpojLEt5ZBAgZCCbUQ9Eof1xnsr8h9d0b3bWY6b6aNqrOTZA7jZC5gKZApbZB0"
         }
-
-
-
     };
 
-    const req=https.request(options,res=>{
+    const req = https.request(options, res => {
+        let responseData = '';
+        
+        res.on("data", chunk => {
+            responseData += chunk;
+        });
 
-        res.on("data",d=>{
-            process.stdout.write(d);
+        res.on("end", () => {
+            console.log("Response from server:", responseData);
         });
     });
 
-    //Procesar si esque ocurre un error
-
-
-    req.on("error",error=>{
-        console.error(error);
+    req.on("error", error => {
+        console.error("Error sending message:", error);
     });
-
 
     req.write(data);
     req.end();
-
-
 }
 
-module.exports={SendMessageWhatsApp};
+module.exports = { SendMessageWhatsApp };
