9aa33114a69eb3956a75911c50133129f3154a20
Chuyini <jmlr231201@gmail.com>
2024-06-15 11:43:37 -0600

    Version SampleFormats v3

--- src/constroller/whatsAppController.js
+++ src/constroller/whatsAppController.js
@@ -37,43 +37,51 @@ const Recived = async (req = request, res = response) => {
             if (text === "text") {
                 var data = samples.SampleText("Hola usuario", number);
                 await whatsappService.SendMessageWhatsApp(data);
+                return res.status(200).send("EVENT_RECEIVED");
             
             } else if (text === "image") {
                 var data = samples.SampleImage(number);
                 await whatsappService.SendMessageWhatsApp(data);
+                return res.status(200).send("EVENT_RECEIVED");
             
             } else if (text === "video") {
                 var data = samples.SampleVideo(number);
                 await whatsappService.SendMessageWhatsApp(data);
+                return res.status(200).send("EVENT_RECEIVED");
             
             } else if (text === "audio") {
                 var data = samples.SampleAudio(number);
                 await whatsappService.SendMessageWhatsApp(data);
+                return res.status(200).send("EVENT_RECEIVED");
             
             } else if (text === "document") {
                 var data = samples.SampleDocument(number);
                 await whatsappService.SendMessageWhatsApp(data);
+                return res.status(200).send("EVENT_RECEIVED");
             
             } else if (text === "button") {
                 var data = samples.SampleButtons(number);
                 await whatsappService.SendMessageWhatsApp(data);
-            
+                return res.status(200).send("EVENT_RECEIVED");
             } else if (text.toLowerCase() === "list") {
                 var data = samples.SampleList(number); // Asumiendo que quieres enviar una lista, no un texto.
                 await whatsappService.SendMessageWhatsApp(data);
+                return res.status(200).send("EVENT_RECEIVED");
             
             } else if (text === "location") {
                 var data = samples.SampleLocation(number);
                 await whatsappService.SendMessageWhatsApp(data);
+                return res.status(200).send("EVENT_RECEIVED");
             
             } else {
                 var data = samples.SampleText("No entiendo", number); // Agregado el segundo parámetro.
                 await whatsappService.SendMessageWhatsApp(data);
+                return res.status(200).send("EVENT_RECEIVED");
             }
             
         }
 
-        return res.status(200).send("EVENT_RECEIVED");
+        
     } catch (error) {
         console.error("Error in Recived function:", error);
         return res.status(500).send("Error processing event.");
