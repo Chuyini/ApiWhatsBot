1911d2a372143e8bfa117e8e8f507b84167f70b1
Chuyini <jmlr231201@gmail.com>
2024-06-19 09:46:11 -0600

    Version estable 28

--- src/constroller/whatsAppController.js
+++ src/constroller/whatsAppController.js
@@ -58,7 +58,7 @@ const Recived = (req = request, res = response) => {
 
             //Una vez procesado se manda la contestación
             console.log("Data being sent:", text);
-            SendMessageWhatsApp(data)
+            whatsappService(data)
                 .then(response => {
                     console.log("Message processed successfully.");
                     console.log("Response from server:", response.statusCode, response.responseData);
