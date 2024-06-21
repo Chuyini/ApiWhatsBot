5ffa7fca73e0b1b721a23eaba357a57c0991c840
Chuyini <jmlr231201@gmail.com>
2024-06-21 16:32:32 -0600

    Integracion con chatGPT de OpenAi v20

---
+++ src/service/chatGPT-service.js
@@ -0,0 +1,27 @@
+const OpenAI = require=("openai");
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function GetMessageChatGPT(text) {
+    try {
+        const stream = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: text }],
+            stream: true,
+        });
+
+        let responseText = '';
+        for await (const chunk of stream) {
+            responseText += chunk.choices[0]?.delta?.content || "";
+        }
+
+        return responseText;
+    } catch (error) {
+        console.error("Error fetching response from OpenAI:", error);
+        return null;
+    }
+}
+
+export { GetMessageChatGPT };
