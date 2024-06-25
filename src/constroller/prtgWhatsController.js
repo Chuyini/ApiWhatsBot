5a3a0192d4580b225f57ad483b318e54cbe934ae
Chuyini <jmlr231201@gmail.com>
2024-06-24 18:31:51 -0600

    Api whats + PRTG V9 formato urlcode

--- src/constroller/prtgWhatsController.js
+++ src/constroller/prtgWhatsController.js
@@ -6,6 +6,7 @@ const Recived = async (req = request, res = response) => {
         console.log("Request body:", JSON.stringify(req.body, null, 2));
 
         const sensorData = req.body;
+        
         console.log(sensorData);
 
         if (!sensorData) {
@@ -13,15 +14,16 @@ const Recived = async (req = request, res = response) => {
             return res.status(400).send("No sensor data found in request.");
         }
         // Extraer y asignar variables de la carga útil
-        
+        const sensorInfo = sensorData.sensor;
+        const lines = sensorInfo.split('\r\n');
 
         // Reemplaza con el número de teléfono de destino
         const number = "524401050937";
 
-        console.log(`Sending message: "${sensorData.sensor}" to number: ${number}`);
+        console.log(`Sending message: "${lines}" to number: ${number}`);
 
         // Llama a la función Process de manera asincrónica
-        await processMessage.ProcessToPrtg(sensorData.sensor, number);
+        await processMessage.ProcessToPrtg(lines, number);
 
         return res.status(200).send("EVENT_RECEIVED");
     } catch (error) {
