cf10e2bab140c65832c47d1df36a545d3e09642d
Chuyini <jmlr231201@gmail.com>
2024-07-03 12:37:37 -0600

    Control preliminar de volumen con queue y bottleneck

--- src/constroller/prtgWhatsController.js
+++ src/constroller/prtgWhatsController.js
@@ -1,10 +1,19 @@
-const {
-    request,
-    response
-} = require("express");
+const { request, response } = require("express");
 const processMessageR = require("../shared/processToPrtg");
 const chatGPTService = require("../service/chatGPT-service");
+const Queue = require('bull');
+const Bottleneck = require('bottleneck');
 
+// Crear una cola de mensajes
+const messageQueue = new Queue('messageQueue');
+
+// Configurar Bottleneck para la limitación de velocidad
+const limiter = new Bottleneck({
+    maxConcurrent: 1,  // Número máximo de tareas concurrentes
+    minTime: 60000 / 100  // Intervalo mínimo de tiempo entre tareas (100 mensajes por minuto)
+});
+
+// Función principal que maneja la recepción de datos del sensor
 const Recived = async (req = request, res = response) => {
     try {
         console.log("Request body:", JSON.stringify(req.body, null, 2));
@@ -17,40 +26,17 @@ const Recived = async (req = request, res = response) => {
             console.error("No sensor data found in request.");
             return res.status(400).send("No sensor data found in request.");
         }
-        // Extraer y asignar variables de la carga útil
-        //le daremos formato segun la informacion
-        //concatenaremos los datos
 
+        // Construir la información del mensaje basada en los datos del sensor
         const sensorInfo = await buildInformation(sensorData);
-        //metemos a la cola  el texto y aplicamos alguna funcion supongo
 
-        // Reemplazar con el número de teléfono de destino
+        // Lista de números de teléfono de destino
         const numbers = ["524401050937", "524434629327", "524442478574"];
 
-        //"524442478574"
-
-
-
-        // Enviar el mensaje a cada número de manera asincrónica
-        // Enviar el mensaje a cada número de manera asincrónica
-        const promises = numbers.map(async (number) => {
-
-            console.log(`Sending message: "${sensorInfo}" to number: ${number}`);
-            console.log("*********************************\n\n");
-            try {
-                await processMessageR.ProcessToPrtg(sensorInfo, number);
-
-                console.log(`Message sent to ${number}`);
-                console.log("*********************************\n\n");
-            } catch (error) {
-
-                console.error(`Failed to send message to ${number}:`, error);
-                console.log("*********************************\n\n");
-            }
+        // Añadir mensajes a la cola
+        numbers.forEach(number => {
+            messageQueue.add({ sensorInfo, number });
         });
-
-        // Esperar a que todas las promesas se resuelvan
-        await Promise.all(promises);
 
         return res.status(200).send("EVENT_RECEIVED");
     } catch (error) {
@@ -59,9 +45,8 @@ const Recived = async (req = request, res = response) => {
     }
 };
 
+// Función para construir la información del mensaje basada en los datos del sensor
 async function buildInformation(sensorData) {
-
-
     const company = sensorData.company;
     const device = sensorData.device;
     const ip = sensorData.ip;
@@ -77,41 +62,24 @@ async function buildInformation(sensorData) {
     let text;
     let AIresponse;
 
-
-
-
-
     if (lowerCaseText.includes("ok")) {
-
         statusEmoji = "🟢";
-
     } else if (lowerCaseText.includes("advertencia")) {
-
         statusEmoji = "⚠️🟢";
-
     }
-
 
     const id = extractNumbersAndText(company);
     linkUisp = concatLink(id);
 
     console.log(id);
 
-    priority = priority.trim(); //quita espacios de la cadena
-
-
-    //la plantilla requiere un nivel de prioridad
-
+    priority = priority.trim();
 
     if (message == undefined || message == null) {
-
-        message ="Posible error de conexión";
+        message = "Posible error de conexión";
     }
 
-
-
     switch (priority) {
-
         case "*":
             priority = "Muy baja";
             break;
@@ -132,77 +100,53 @@ async function buildInformation(sensorData) {
             break;
     }
 
-
-
-
-
     if (sensorData.batery) {
-
-        //PRTG de baterias
-
-        //alguna condicion si ya levanto
-
-
-        text = `BATERIAS URGENTE:\n🏢EMPRESA/LUGAR: *${company}*\n\nDISPOSITIVO: *${device}*\n\n${statusEmoji}ESTADO:*${status}*\n\n🌐IP: *${ip}* \n\nTIEMPO: *${time}*\n\nPRIORIDAD: *${priority}* `
+        text = `BATERIAS URGENTE:\n🏢EMPRESA/LUGAR: *${company}*\n\nDISPOSITIVO: *${device}*\n\n${statusEmoji}ESTADO:*${status}*\n\n🌐IP: *${ip}* \n\nTIEMPO: *${time}*\n\nPRIORIDAD: *${priority}* `;
         return text;
-    } else { //PRTG de clientes
-
-        
-        //alguna ccondicion si ya levanto
-
-        //alguna condicion si es de comunicalo
-
+    } else {
         if (lowerCaseComuni.includes("comunicalo") && !/^192\.168\./.test(lowerCaseIp)) {
-
-            AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo  no agregues codigos de error, además pregunta si sucede algo con la electricidad o alguna afectacion ya que es comunicalo y ellos son un isp. No agreges emogies :"+message)
-
+            AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo no agregues codigos de error, además pregunta si sucede algo con la electricidad o alguna afectacion ya que es comunicalo y ellos son un isp. No agreges emogies :" + message)
             text = `\n🏢 *${company}*\n\nSERVICIO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\n${AIresponse}`;
-
-
-
         } else {
-
-            AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo  no agrueges codigos de error y pon emogies mas corto de lo que es el propio mensaje:"+message)
+            AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo no agrueges codigos de error y pon emogies mas corto de lo que es el propio mensaje:" + message)
             text = `Sensor Alert:\n🏢 EMPRESA/LUGAR: *${company}*\n\nDISPOSITIVO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\nPRIORIDAD: *${priority}*\n\n${AIresponse}\n\n🔗 LINK UISP: *${linkUisp}*`;
-
         }
-
-
         return text;
     }
-
-
-
 }
 
-
+// Función para extraer números y texto del nombre de la empresa
 function extractNumbersAndText(text) {
     const match = text.match(/^(\d+)\s*-\s*(.*)/);
-
     if (match) {
         return match[1];
-
     } else {
         return null; // Devuelve null si no se encuentran coincidencias
     }
 }
 
+// Función para concatenar el enlace UISP basado en el ID
 function concatLink(id) {
     if (id) {
-
-
         return linkUisp = "https://uisp.elpoderdeinternet.mx/crm/client/" + id;
-
     } else {
-
         return linkUisp = "https://uisp.elpoderdeinternet.mx/crm";
-
     }
-
 }
 
+// Procesar trabajos en la cola
+messageQueue.process(async (job) => {
+    const { sensorInfo, number } = job.data;
 
-
+    await limiter.schedule(async () => {
+        try {
+            await processMessageR.ProcessToPrtg(sensorInfo, number);
+            console.log(`Message sent to ${number}`);
+        } catch (error) {
+            console.error(`Failed to send message to ${number}:`, error);
+        }
+    });
+});
 
 module.exports = {
     Recived
