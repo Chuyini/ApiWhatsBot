f809e4364ca2a2ae67faa881e030229e8526d799
Chuyini <jmlr231201@gmail.com>
2024-07-02 18:32:29 -0600

    Formato para comunicalo sin link ni prioridad v3 mensaje incluido

--- src/constroller/prtgWhatsController.js
+++ src/constroller/prtgWhatsController.js
@@ -150,7 +150,7 @@ function buildInformation(sensorData) {
 
         if (lowerCaseComuni.includes("comunicalo") && !/^192\.168\./.test(lowerCaseIp)) {
 
-            text = `\n🏢 EMPRESA/LUGAR: *${company}*\n\nSERVICIO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\n${message}`;
+            text = `\n🏢 *${company}*\n\nSERVICIO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\n${message}`;
 
 
 
