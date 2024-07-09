f14b6dbeb5335068b6b2c795f444258a777ee61e
Chuyini <jmlr231201@gmail.com>
2024-07-09 14:49:46 -0600

    include when it is batery

--- src/constroller/prtgWhatsController.js
+++ src/constroller/prtgWhatsController.js
@@ -19,25 +19,17 @@ const limiter = new Bottleneck({
 
 const Recived = async (req = request, res = response) => {
     try {
-        //console.log("Request body:", JSON.stringify(req.body, null, 2));
-
         const sensorData = req.body;
 
         if (!sensorData) {
             console.error("No sensor data found in request.");
             return res.status(400).send("No sensor data found in request.");
         }
-        //Extraer y asignar variables de la carga útil
-        //Le daremos formato segun la informacion
-        //Concatenaremos los datos
 
-        const sensorInfo = await buildInformation(sensorData);
-        //Metemos a la cola  el texto y aplicamos alguna funcion supongo
-
-
-        //Reemplazar con el número de teléfono de destino
-        const numbers = ["524401050937", "524434629327", "524442478574"];
-
+        const {
+            text: sensorInfo,
+            numbers
+        } = await buildInformation(sensorData);
 
         numbers.forEach(number => {
             messageQueue.add({
@@ -46,29 +38,19 @@ const Recived = async (req = request, res = response) => {
             });
         });
 
-
-        
-
-
-        //Enviar el mensaje a cada número de manera asincrónica
         const promises = numbers.map(async (number) => {
-
             console.log(`Sending message: "${sensorInfo}" to number: ${number}`);
             console.log("*********************************\n\n");
             try {
-
                 await processMessageR.ProcessToPrtg(sensorInfo, number);
-
                 console.log(`Message sent to ${number}`);
                 console.log("*********************************\n\n");
             } catch (error) {
-
                 console.error(`Failed to send message to ${number}:`, error);
                 console.log("*********************************\n\n");
             }
         });
 
-        //Esperar a que todas las promesas se resuelvan
         await Promise.all(promises);
 
         return res.status(200).send("EVENT_RECEIVED");
@@ -79,8 +61,6 @@ const Recived = async (req = request, res = response) => {
 };
 
 async function buildInformation(sensorData) {
-
-
     const company = sensorData.company;
     const device = sensorData.device;
     const ip = sensorData.ip;
@@ -96,43 +76,24 @@ async function buildInformation(sensorData) {
     let text;
     let AIresponse;
     let idUispService = extractNumberFromCompany(company);
-
+    const numbers = ["524401050937", "524442478574"];
     console.log("\n 🔴El codigo de concatenacion \n");
 
-
-
-
-
     if (lowerCaseText.includes("fallo finalizado")) {
-
         statusEmoji = "🟢";
-
     } else if (lowerCaseText.includes("anterior :advertencia")) {
-
         statusEmoji = "⚠️🟢";
-
     }
-
 
     const id = extractNumbersAndText(company);
     linkUisp = concatLink(idUispService);
-
-
-    priority = priority.trim(); //quita espacios de la cadena
-
-
-    //la plantilla requiere un nivel de prioridad
-
+    priority = priority.trim();
 
     if (message == undefined || message == null) {
-
         message = "Posible error de conexión";
     }
 
-
-
     switch (priority) {
-
         case "*":
             priority = "Muy baja";
             break;
@@ -153,73 +114,44 @@ async function buildInformation(sensorData) {
             break;
     }
 
-
-
-
-
     if (sensorData.batery) {
-
-        //PRTG de baterias
-
-        //alguna condicion si ya levanto
-
-
-        text = `BATERIAS URGENTE:\n🏢EMPRESA/LUGAR: *${company}*\n\nDISPOSITIVO: *${device}*\n\n${statusEmoji}ESTADO:*${status}*\n\n🌐IP: *${ip}* \n\nTIEMPO: *${time}*\n\nPRIORIDAD: *${priority}* `
-        return text;
-    } else { //PRTG de clientes
-
-
-        //alguna ccondicion si ya levanto
-
-        //alguna condicion si es de comunicalo
-
+        text = `BATERIAS URGENTE:\n🏢EMPRESA/LUGAR: *${company}*\n\nDISPOSITIVO: *${device}*\n\n${statusEmoji}ESTADO:*${status}*\n\n🌐IP: *${ip}* \n\nTIEMPO: *${time}*\n\nPRIORIDAD: *${priority}* `;
+        return {
+            text,
+            numbers
+        };
+    } else {
         if (lowerCaseComuni.includes("comunicalo") && !/^192\.168\./.test(lowerCaseIp)) {
-
-            AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo  no agregues codigos de error, además pregunta si sucede algo con la electricidad o alguna afectacion ya que es comunicalo y ellos son un isp. No agreges emogies :" + message)
-
+            AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo no agregues codigos de error, además pregunta si sucede algo con la electricidad o alguna afectacion ya que es comunicalo y ellos son un isp. No agreges emogies :" + message);
             text = `\n🏢 *${company}*\n\nSERVICIO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\n${AIresponse}`;
-
-
-
+            numbers.push("524434629327");
         } else {
-
-            AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo  no agrueges codigos de error y pon emogies mas corto de lo que es el propio mensaje:" + message)
+            AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo no agrueges codigos de error y pon emogies mas corto de lo que es el propio mensaje:" + message);
             text = `Sensor Alert:\n🏢 EMPRESA/LUGAR: *${company}*\n\nDISPOSITIVO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\nPRIORIDAD: *${priority}*\n\n${AIresponse}\n\n🔗 LINK UISP: *${linkUisp}*`;
-
         }
 
-
-        return text;
+        return {
+            text,
+            numbers
+        };
     }
-
-
-
 }
-
 
 function extractNumbersAndText(text) {
     const match = text.match(/^(\d+)\s*-\s*(.*)/);
-
     if (match) {
         return match[1];
-
     } else {
-        return null; // Devuelve null si no se encuentran coincidencias
+        return null;
     }
 }
 
 function concatLink(id) {
     if (id) {
-
-
-        return linkUisp = "https://uisp.elpoderdeinternet.mx/crm/client/" + id;
-
+        return "https://uisp.elpoderdeinternet.mx/crm/client/" + id;
     } else {
-
-        return linkUisp = "https://uisp.elpoderdeinternet.mx/crm";
-
+        return "https://uisp.elpoderdeinternet.mx/crm";
     }
-
 }
 
 function extractNumberFromCompany(company) {
@@ -227,8 +159,6 @@ function extractNumberFromCompany(company) {
     const match = company.match(regex);
     return match ? match[1] : null;
 }
-
-
 
 module.exports = {
     Recived
