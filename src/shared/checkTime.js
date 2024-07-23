ec12b224a897927af7f86d8cd64c2224a993f2a1
Chuyini <jmlr231201@gmail.com>
2024-07-23 16:48:06 -0600

    setinterval is in app listened

--- src/shared/checkTime.js
+++ src/shared/checkTime.js
@@ -4,9 +4,7 @@ const whatsAppService = require("../service/whatsappService");
 //let lastExecutionTime = null;
 
 async function checkTimeAndGreet() {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
+    
 
     let models = [];
     const numbers = ["524401050937"];
@@ -16,19 +14,19 @@ async function checkTimeAndGreet() {
         models.push(model);
     }
 
-    if (true) {
-        console.log("Enviando mensajes a las 6 PM...");
-        try {
-            for (const element of models) {
-                const response = await whatsAppService.SendMessageWhatsApp(element);
-                console.log("Message processed successfully.");
-                console.log("Response from server:", response.statusCode, response.responseData);
-                console.log("Message sent successfully.");
-            }
-        } catch (error) {
-            console.error("Error sending message:", error);
+
+    console.log("Enviando mensajes a las 6 PM...");
+    try {
+        for (const element of models) {
+            const response = await whatsAppService.SendMessageWhatsApp(element);
+            console.log("Message processed successfully.");
+            console.log("Response from server:", response.statusCode, response.responseData);
+            console.log("Message sent successfully.");
         }
+    } catch (error) {
+        console.error("Error sending message:", error);
     }
+
 }
 
 module.exports = {
