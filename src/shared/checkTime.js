49a4dd9c2c6cef9ee3e144402209bd0ef6290bb1
Chuyini <jmlr231201@gmail.com>
2024-11-26 11:41:21 -0600

    add template battery

--- src/shared/checkTime.js
+++ src/shared/checkTime.js
@@ -26,7 +26,7 @@ async function checkTimeAndGreet(numbers,textBuilt) {
             console.log("Message sent successfully.");
         }
     } catch (error) {
-        console.error("Error sending message:", error);
+        console.error("TUVIMOS UN ERROR EN LAS PLANTILLAS: ", error);
     }
 
 }
