2c7c6a72ff70844051fd3783e2a7aac456635e2f
Chuyini <jmlr231201@gmail.com>
2024-12-23 15:31:41 -0600

    INCLUDE CREATE TICKET BETA

--- src/constroller/prtgWhatsController.js
+++ src/constroller/prtgWhatsController.js
@@ -201,7 +201,10 @@ async function buildInformation(sensorData) {
         if (lowerCaseComuni.includes("comunicalo") || tags.includes("comunicalo") /*&& !/^192\.168\./.test(lowerCaseIp)*/) {
             AIresponse = await chatGPTService.GetMessageChatGPT("Puedes resumir lo siguiente es para mandarlo como reporte solo pon algo sencillo no agregues codigos de error, además pregunta si sucede algo con la electricidad o alguna afectacion ya que es comunicalo y ellos son un isp. No agreges emogies :" + message);
             text = `\n🏢 *${company}*\n\nSERVICIO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\n${AIresponse}\n\n${comments}`;
-            await ticketUisp.createTicketUisp(sensorData,text);
+            if(lowerCaseText.includes("fallo") && statusEmoji == "🔴"){
+                await ticketUisp.createTicketUisp(sensorData,text);
+            }
+            
         } else {
             // AIresponse = await chatGPTService.GetMessageChatGPT(message); <-- no necesitamos algun reporte cuando este en OK
             text = `Sensor Alert ${statusEmoji}:\n🏢 EMPRESA/LUGAR: *${company}*\n\nDISPOSITIVO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\nPRIORIDAD: *${priority}*\n\n${message}\n\n🔗 LINK UISP: *${linkUisp}*\n\n ${comments}\n\n etiquetas: ${tags}`;
