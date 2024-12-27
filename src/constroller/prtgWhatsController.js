dda8f0aa915492a19a26aa51e6315d770cc53a59
Chuyini <jmlr231201@gmail.com>
2024-12-27 16:05:14 -0600

    Check if there is a ticket on UISP

--- src/constroller/prtgWhatsController.js
+++ src/constroller/prtgWhatsController.js
@@ -214,8 +214,9 @@ async function buildInformation(sensorData) {
             text = `Sensor Alert ${statusEmoji}:\n🏢 EMPRESA/LUGAR: *${company}*\n\nDISPOSITIVO: *${device}*\n\n${statusEmoji} ESTADO: *${status}*\n\n🌐 IP: *${ip}*\n\nTIEMPO: *${time}*\n\nPRIORIDAD: *${priority}*\n\n${message}\n\n🔗 LINK UISP: *${linkUisp}*\n\n ${comments}\n\n etiquetas: ${tags}`;
             if(lowerCaseText.includes("repetir escalacion")){//si no es de comunicalo pero es un repetir escalacion
                 let hasTicket = await foundTicket.isThereTicketOnUisp(sensorData);
+                console.log("esto dio la resupuesta : ",hasTicket);
 
-                if(! hasTicket){
+                if(!hasTicket){
 
                     await ticketUisp.createTicketUisp(sensorData,text);
 
