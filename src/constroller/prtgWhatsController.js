d47627b8ab8f522f6c4d7dc3e0b3c730383376ff
Chuyini <jmlr231201@gmail.com>
2025-01-17 13:45:31 -0600

    include cache node

--- src/constroller/prtgWhatsController.js
+++ src/constroller/prtgWhatsController.js
@@ -344,6 +344,8 @@ function extractNumberFromCompany(company) {
 
 async function masiveFaildBuild(statusAndDevices) {
 
+    console.log("status and devices ",statusAndDevices);
+
     const devicesAlarmed = statusAndDevices.devices;
     const numbers = ["524442475444", "524441967796", "524441574990", "524441184908", "524434629327", "524442478772"];
 
