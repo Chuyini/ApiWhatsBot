28af376b5afd9fc345747ddeccf00a863370a0ee
chuyini <jmlr231201@gmail.com>
2025-07-16 12:06:04 -0600

    ok

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,7 +11,7 @@ router.get("/", whatsappController.VerifyToken); //para recibir el token tiene q
 router.post("/", whatsappController.Recived); //y para recibir lo mensajes
 router.post("/prtg", prtgController.Recived); //y para recibir lo mensajes de prtg y mandarlos
 router.post("/tickets",railwayController.doTickets);//<-- aqui railway o cualquier servidor notifica que debemos ya hacer los tickets porque ya terminó
-router.post("/whatsApp/telnyx", telnyxController.alertaRadiobase); //para recibir eventos de telnyx
+router.post("/webhook/telnyx", telnyxController.alertaRadiobase); //para recibir eventos de telnyx
 
 
 module.exports = router;
