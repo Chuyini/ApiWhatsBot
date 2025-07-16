9f21934525a4d90d1927ea047306c11e254facdf
chuyini <jmlr231201@gmail.com>
2025-07-16 16:59:37 -0600

    call control

--- src/constroller/telnyx.js
+++ src/constroller/telnyx.js
@@ -81,7 +81,29 @@ const alertaRadiobase = async (req, res) => {
   }
 };
 
+
+
+
+const enviarMensajeTTS = async (callControlId, texto) => {
+  const telnyx = await import('telnyx').then(mod => mod.default(process.env.TELNYX_KEY));
+
+  if (!callControlId || !texto) throw new Error("Faltan datos para enviar TTS");
+
+  await telnyx.calls.speak({
+    call_control_id: callControlId,
+    payload: {
+      voice: "female",
+      language: "es-MX",
+      text: texto
+    }
+  });
+
+  console.log("🔊 Mensaje TTS enviado correctamente");
+};
+
+
 module.exports = {
   recibirEventoTelnyx,
-  alertaRadiobase
+  alertaRadiobase,
+  enviarMensajeTTS
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,6 +12,7 @@ router.post("/", whatsappController.Recived); //y para recibir lo mensajes
 router.post("/prtg", prtgController.Recived); //y para recibir lo mensajes de prtg y mandarlos
 router.post("/tickets",railwayController.doTickets);//<-- aqui railway o cualquier servidor notifica que debemos ya hacer los tickets porque ya terminó
 router.post("/webhook/telnyx", telnyxController.alertaRadiobase); //para recibir eventos de telnyx
+router.post("/webhook/tts", telnyxController.enviarMensajeTTS); //para recibir eventos de telnyx
 
 
 module.exports = router;
