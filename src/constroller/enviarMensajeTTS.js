155a9865eb0bc9a0f2b061e672edde59fd0fc2d9
chuyini <jmlr231201@gmail.com>
2025-07-16 17:52:18 -0600

    call control

--- src/constroller/enviarMensajeTTS.js
+++ src/constroller/enviarMensajeTTS.js
@@ -5,13 +5,11 @@ const enviarMensajeTTS = async (callControlId, texto) => {
     const mensaje = "Hola, este es un aviso automático del sistema PRTG Network Monitos. Radiobase GR08 ha detectado una actividad irregular en la zona de operación. Por favor, revisa la alerta en tu panel técnico para confirmar el estado.";
     await telnyx.calls.speak({
         call_control_id: callControlId,
-        payload: "Hola, este es un aviso automático de PoderNET. Radiobase GR08 ha reportado actividad irregular.",
-        payload_type: "text",           // ← opcional, por defecto es "text"
-        service_level: "premium",       // ← recomendable para mejor calidad
-        voice: "female",                // ← para nivel básico; o usa Telnyx.Neural.EsMx_01 para premium
-        language: "es-MX"               // ← importante si usas voz genérica tipo "female"
+        payload: "This is a basic test",
+        service_level: "basic",
+        voice: "female",
+        language: "en-US"
     });
-
     console.log("🔊 Mensaje TTS enviado correctamente");
 };
 
